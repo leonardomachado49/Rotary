43faa4d5e6902accea1e1cfcba392a0b6d24973d
leonardotexiera49 <leonardozb3@gmail.com>
2016-10-25 15:45:16 -0200

    Rotary

---
+++ application/controllers/mainController.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MainController extends CI_Controller {
+
+	public function index()
+	{
+		$data['categorys'] = $this->CategoriaModel->getCategorys();
+		$this->template->load('template/template','main/mainView',$data);		
+	}
+
+
+	public function rotaryData(){
+		$categories = $this->CategoriaModel->getCategories();
+		foreach ($categories as $category) {
+			$category['itens'] = $this->ItensModel->getItensByCategory($category['category_id']);
+			echo json_encode($category);
+		}
+	}
+}
+
+/* End of file mainController.php */
+/* Location: ./application/controllers/mainController.php */
